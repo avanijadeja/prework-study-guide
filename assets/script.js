e7a46abceb56bf10de6037ff31b53a809a0a7e2a
Avani <avani.jadeja@gmail.com>
2023-01-29 20:48:24 -0600

    < array and for loop concepts in javascript>

---
+++ assets/script.js
@@ -0,0 +1,41 @@
+
+var topics =['HTML','CSS','Git','Javascript'];
+
+for(var x=0; x<topics.length; x++){
+    console.log(topics[x]);
+}
+
+
+
+
+
+
+/*
+var topic="HTML";
+
+if(topic === 'HTML')
+{
+    console.log("Let's study HTML!");
+
+}
+else if(topic === 'CSS')
+{
+    console.log("Let's study CSS!");
+
+}  
+
+else if(topic === 'GIT')
+{
+    console.log("Let's study GIT!");
+
+}    
+else if(topic === 'Javascript')
+{
+    console.log("Let's study Javascript!");
+
+}
+
+else {
+    console.log('please try again');
+}
+*/
